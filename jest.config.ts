7cb055e786497a541f1ee31c3c2f23bff7d51100
Andres Sanabria <andrs.dev@gmail.com>
2020-12-23 12:44:03 -0600

    removed comments

--- jest.config.ts
+++ jest.config.ts
@@ -1,9 +1,8 @@
-// jest.config.ts
 import type { Config } from '@jest/types';
 
-// Sync object
 const config: Config.InitialOptions = {
   preset: 'ts-jest',
   verbose: true,
 };
+
 export default config;

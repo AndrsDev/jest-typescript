8d56e5b3b1633b4d28813a57acf6fd5c02519c0d
Andres Sanabria <andrs.dev@gmail.com>
2020-12-23 12:42:48 -0600

    updated jest config with typescript

--- jest.config.ts
+++ jest.config.ts
@@ -1,4 +1,9 @@
-module.exports = {
+// jest.config.ts
+import type { Config } from '@jest/types';
+
+// Sync object
+const config: Config.InitialOptions = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
+  verbose: true,
 };
+export default config;
